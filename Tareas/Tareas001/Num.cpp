b310fc43b8869847cf8dc31cfae9375af647b1a3
Leonardo Barrantes Mora <barrantesleonardo54@gmail.com>
2025-02-11 21:56:21 -0600

    Archivos de la primera tarea de programcion
    Signed-off-by: Leonardo Barrantes Mora <barrantesleonardo54@gmail.com>

---
+++ Tareas/Tareas001/Num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1,num2;
+    cout <<"ingrse un numero  ";
+    cin >> num1;
+    cout <<"ingrese otro numero ";
+    cin >> num2;
+    cout << "Suma:" <<num1 + num2 << endl;
+    cout << "Multiplicacion:" <<num1*num2   <<endl;
+
+}
+
+
---
+++ Tareas/Tareas001/Readme.md
@@ -0,0 +1,11 @@
+# TAREA 1 
+En este readme se esta indicando los dos programas de la tarea
+
+## hello_world.cpp
+
+g++ hello_world.cpp -o hello_world
+./hello_world
+
+## Num.cpp
+g++ Num.cpp -o Num
+./Num
